c06b21c295366aae4c1f1c21f9e3ab8226c54555
gcarloin2002 <gcarloin2002@gmail.com>
2024-10-01 11:46:31 -0500

    branch

--- frontend/src/app/Registration/page.tsx
+++ frontend/src/app/Registration/page.tsx
@@ -2,11 +2,36 @@ import Link from 'next/link';
 import "./styles.css";
 
 export default function RegistrationPage() {
+
+
+  
   return (
     <div className="page">
       <h1>Registration Page</h1>
       <Link href="/">Go to Welcome Page</Link>
       <Link href="/Login">Go to Login Page</Link>
+
+
+      <form>
+        <label>Username or Password</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input type="submit" value="Login" />
+      </form>
+
+
     </div>
   );
 }
