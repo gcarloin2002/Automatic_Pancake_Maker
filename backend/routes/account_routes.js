d5bf34be84c25107930ec307b850e0f1768bfe03
mohsin <87213341+mohsinkhan25@users.noreply.github.com>
2024-10-20 19:14:45 -0500

    Got username added to nextauth session

--- backend/routes/account_routes.js
+++ backend/routes/account_routes.js
@@ -76,7 +76,7 @@ router.post('/login', async function login(req, res) {
 
     if (hashedPassword === account.account_password) {
       // Include role in the response
-      res.status(200).json({ message: 'Login successful', role: account.role });
+      res.status(200).json({ message: 'Login successful', role: account.role, username: account.account_username });
     } else {
       res.status(401).json({ message: 'Invalid login or password' });
     }
