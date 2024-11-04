f021f6688965dd2d96c5f0da3fbce8faf4c3bf31
gcarloin2002 <gcarloin2002@gmail.com>
2024-11-03 19:38:00 -0600

    setup CSS for home page

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <div className="page">
+    <>
       <h1>Home Page</h1>  
       <LogoutButton />
       <Link href="/">Go to Welcome Page</Link>
       <Link href="/Order">Go to Order Page</Link>
       <Link href="/Queue">Go to Queue Page</Link>
       <Link href="/Diagnostics">Go to Diagnostics Page</Link>
-    </div>
+    </>
   );
 }
