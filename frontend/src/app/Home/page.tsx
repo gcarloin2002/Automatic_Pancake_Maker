124353f04985917ca164b9d440c9c354d0976e62
gcarloin2002 <gcarloin2002@gmail.com>
2024-11-03 21:35:53 -0600

    home page progress

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -1,15 +1,58 @@
+"use client";
+
 import LogoutButton from '@/components/LogoutButton';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import '../../styles/Home.css';
 
 export default function HomePage() {
+
+  const router = useRouter();
+
+
+
+  const order_click = () => {
+    router.push('/Order');  // Redirect to Registration page
+  };
+
+  const queue_click = () => {
+    router.push('/Queue');  // Redirect to Registration page
+  };
+
+  const diagnostics_click = () => {
+    router.push('/Diagnostics');  // Redirect to Registration page
+  };
+
+
   return (
     <>
-      <h1>Home Page</h1>  
-      <LogoutButton />
-      <Link href="/">Go to Welcome Page</Link>
-      <Link href="/Order">Go to Order Page</Link>
-      <Link href="/Queue">Go to Queue Page</Link>
-      <Link href="/Diagnostics">Go to Diagnostics Page</Link>
+      <div className="home-top-bar-gap"></div>
+      <div className="home-top-bar">
+        <LogoutButton />
+      </div>
+
+      <div className="home-buttons-container">
+        <div className="home-buttons">
+          <button className="home-button home-button-1" onClick={order_click}>
+            <h1 className="home-button-label">
+              Order
+            </h1>
+          </button>
+          <button className="home-button home-button-2" onClick={queue_click}>
+            <h1 className="home-button-label">
+              Queue
+            </h1>
+          </button>
+          <button className="home-button home-button-3" onClick={diagnostics_click}>
+            <h1 className="home-button-label">
+              Diagnostics
+            </h1>
+          </button>
+        </div>
+
+      </div>
+
+
+
     </>
   );
 }
