0f8cf2a9beff85d7ab74e5fb1ec37f1d6a2a1389
gcarloin2002 <gcarloin2002@gmail.com>
2024-11-04 16:50:35 -0600

    added icons to home page

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import Image from 'next/image';
 import LogoutButton from '@/components/LogoutButton';
 import { useRouter } from 'next/navigation';
 import '../../styles/Home.css';
+import order_icon from '../../assets/order-icon.png';
+import queue_icon from '../../assets/queue-icon.png';
+import diagnostics_icon from '../../assets/diagnostics-icon.png';
 
 export default function HomePage() {
 
@@ -28,25 +32,30 @@ export default function HomePage() {
       <div className="home-top-bar-gap"></div>
       <div className="home-top-bar">
         <LogoutButton />
+        <h1>Welcome User!</h1>
+        <div></div>
       </div>
 
       <div className="home-buttons-container">
 
         <div className="home-buttons">
           <button className="home-button home-button-1" onClick={order_click}>
-            <h1 className="home-button-label">
-              Order
-            </h1>
+            <div className="home-img-container">
+              <Image src={order_icon} alt="Logo" layout="responsive" objectFit="contain" />
+            </div>
+            <h1 className="home-button-label">Order</h1>
           </button>
           <button className="home-button home-button-2" onClick={queue_click}>
-            <h1 className="home-button-label">
-              Queue
-            </h1>
+            <div className="home-img-container">
+              <Image src={queue_icon} alt="Logo" width='250' />
+            </div>
+            <h1 className="home-button-label"> Queue</h1>
           </button>
           <button className="home-button home-button-3" onClick={diagnostics_click}>
-            <h1 className="home-button-label">
-              Diagnostics
-            </h1>
+            <div className="home-img-container">
+              <Image src={diagnostics_icon} alt="Logo" layout="responsive" objectFit="contain" />
+            </div>
+            <h1 className="home-button-label">Diagnostics</h1>
           </button>
 
         </div>
