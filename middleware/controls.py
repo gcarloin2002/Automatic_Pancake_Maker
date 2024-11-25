92008e3aa41af915ce6db36afef53d5d985a82a5
Mohsin Khan <mohsin.ik23@gmail.com>
2024-11-25 00:52:42 +0000

    updating middleware

--- middleware/controls.py
+++ middleware/controls.py
@@ -1,9 +1,33 @@
+#imports
 from gpiozero import AngularServo
 from time import sleep
 from gpiozero.pins.pigpio import PiGPIOFactory
 import pigpio
 
-# 
+#Temp sensor
+import board
+import busio as io
+import adafruit_mlx90614
+from time import sleep
+
+#weight sensor
+import time
+import RPi.GPIO as GPIO                # Import GPIO
+from hx711 import HX711                # Import the HX711 class
+# around 5mm for 20seconds
+# roughly 100grams per pancake set
+
+# source servo_env/bin/activate
+# sudo pigpiod
+
+# Weight sensor setup
+GPIO.setmode(GPIO.BCM) # Set GPIO pin mode to BCM numbering
+hx = HX711(dout_pin=16, pd_sck_pin=6) # Initialize HX711 with pins
+start_raw = 291500 # Set the reference unit (calibrated scale ratio)
+raw_to_gram = 99
+#######
+
+# CONSTANTS
 OILPIN = 22
 SERVOPIN = 17
 PUMPPIN = 27
@@ -27,6 +51,35 @@ pi.write(OILPIN, 1)  # Set pin high (1) or low (0)
 pi.write(OILPIN, 0)
 pi.write(PUMPPIN, 0)
 
+def getTemp():
+    i2c = io.I2C(board.SCL, board.SDA, frequency=100000)
+    mlx = adafruit_mlx90614.MLX90614(i2c)
+
+    ambientTemp = "{:.2f}".format(mlx.ambient_temperature*(9.0/5.0)+32)
+    targetTemp = "{:.2f}".format(mlx.object_temperature*(9.0/5.0)+32)
+
+    sleep(1)
+
+    print("Ambient Temperature:", ambientTemp, "°F")
+    print("Target Temperature:", targetTemp,"°F")
+    return [ambientTemp, targetTemp]
+    sleep(2)
+
+def getWeight():
+    # Get weight in grams
+    weight = hx.get_weight_mean(20)  # Average over 10 readings for stability
+    #weight = hx.get_raw_data_mean()
+    print("raw weight is ", weight)
+    if(weight == False): return -1.0
+    difference = abs(weight) - abs(start_raw)
+    print("diff ", difference)
+    weight_grams = difference / 99
+    print(f"Weight: {weight_grams:.2f} grams")
+    return weight_grams
+    time.sleep(1)  # Delay between readings
+    
+    
+
 # sweeps servo from current angle to angle using delay seconds between each degree
 def sweep(delay, angle):
     # 242 degrees is angle of pan on hotplate and 55 is dropping position
@@ -40,6 +93,7 @@ def sweep(delay, angle):
         sleep(delay)
 
 # spray oil for delay seconds
+# We tested for oil around 8 seconds
 def oil(delay):
     print(f"Spraying oil for {delay} seconds...")
     pi.write(OILPIN, 0)
@@ -50,23 +104,52 @@ def oil(delay):
     sleep(0.1)
     pi.write(OILPIN, 1)
 
-# pump batter for delay seconds
+def oilToggle():
+    pi.write(OILPIN, 0)
+    sleep(0.1)
+    pi.write(OILPIN, 1)
+
+# pump batter for delay seconds 
+# We found the best pump time for 2 pancakes is 17 secs
 def pump(delay):
     print(f"Pumping for {delay} seconds...")
     pi.write(PUMPPIN, 1)
     sleep(delay)
     pi.write(PUMPPIN, 0)
 
+# We found the best flip delay was 0.66
 def flip(delay):
     sweep(0, DROP_ANGLE)
     sleep(delay)
     sweep(0, DROP_ANGLE + 35)
+    sleep(0.1)
+    sweep(0.01, DROP_ANGLE)
     sleep(5)
     sweep(0.01, HOTPLATE_ANGLE)
+
+def sequence():
+    oil(10)    # oil for 2 seconds
+    sleep(2)
+
+    # double pump
+    pump(20)  # pump for 20 seconds
+    sleep(30)
+    pump(10)
+
+    # cook
+    half_min = 3*2
+    for i in range(half_min):
+         sleep(30)
+         print(f"{(i+1)*30} seconds into cook...")
+
+    # flip
+    flip(0.66)
+    
 
 # gently set down
 sweep(0.01, HOTPLATE_ANGLE)
 print("INIT COMPLETE")
+# oilToggle()
 
 ###### MAIN SEQUENCE ######
 
@@ -74,21 +157,23 @@ if __name__=="__main__":
     print("INIT COMPLETE, STARTING SEQUENCE")
     
 
-    # oil(6)    # oil for 2 seconds
+    # oil(8)    # oil for 2 seconds
     # sleep(2)
-    # pump(17)  # pump for 20 seconds
+
+    # # double pump
+    # pump(20)  # pump for 20 seconds
+    # sleep(30)
+    # pump(10)
 
     # # cook
-    # half_min = 4*2
+    # half_min = 3*2
     # for i in range(half_min):
-    #     sleep(30)
-    #     print(f"{(i+1)*30} seconds into cook...")
+    #      sleep(30)
+    #      print(f"{(i+1)*30} seconds into cook...")
 
 
     # # flip
-    # sweep(0, DROP_ANGLE)
-    # sleep(2)
-    # sweep(0.01, HOTPLATE_ANGLE)
+    # flip(0.66)
 
     # pi.stop()
 
--- middleware/scale_go.py
+++ middleware/scale_go.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM)
 hx = HX711(dout_pin=16, pd_sck_pin=6)
 
 # Set the reference unit (calibrated scale ratio)
-start_raw = 291628
+start_raw = 291500
 raw_to_gram = 99
 #reference_unit = -3037.75 
 #hx.set_scale_ratio(reference_unit)
@@ -19,11 +19,12 @@ print("Scale is ready! Continuously printing weight readings...")
 try:
     while True:
         # Get weight in grams
-        #weight = hx.get_weight_mean(10)  # Average over 10 readings for stability
-        weight = hx.get_raw_data_mean()
-        print("weight is", weight)
+        weight = hx.get_weight_mean(20)  # Average over 10 readings for stability
+        #weight = hx.get_raw_data_mean()
+        print("raw weight is ", weight)
+        if(weight == False): continue
         difference = abs(weight) - abs(start_raw)
-        print("diff", difference)
+        print("diff ", difference)
         weight_grams = difference / 99
         print(f"Weight: {weight_grams:.2f} grams")
         time.sleep(1)  # Delay between readings
